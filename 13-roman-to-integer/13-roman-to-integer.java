7d5eacd40f5fe7b7559e3b134a3f3bc0fd83c077
Erik Chin <91162305+echin044@users.noreply.github.com>
2022-07-18 23:38:13 -0400

    Time: 7 ms (70.73%), Space: 45.8 MB (54.45%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -0,0 +1,78 @@
+class Solution {
+    public int romanToInt(String s) {
+        int sum = 0;
+        for(int i = 0; i<s.length();i++){
+            
+            
+            
+            //gets numeric value associated to roman numerals, for current numeral and then next one
+            int value = 0;
+            switch(s.charAt(i)){
+               case 'I':
+                   value = 1;
+                   break;
+               case 'V':
+                   value = 5;
+                   break;
+               case 'X':
+                   value =10;
+                   break;
+               case 'L':
+                   value =50;
+                   break;
+               case 'C':
+                   value =100;
+                   break;
+               case 'D':
+                   value =500;
+                   break;
+               case 'M':
+                   value =1000;
+                   break;
+            }
+            
+            if(i == s.length()-1){
+                sum+=value;
+                continue;
+            }
+            
+            int value2 = 0;        
+            switch(s.charAt(i+1)){
+               case 'I':
+                   value2 = 1;
+                   break;
+               case 'V':
+                   value2 = 5;
+                   break;
+               case 'X':
+                   value2 =10;
+                   break;
+               case 'L':
+                   value2 =50;
+                   break;
+               case 'C':
+                   value2 =100;
+                   break;
+               case 'D':
+                   value2 =500;
+                   break;
+               case 'M':
+                   value2 =1000;
+                   break;
+            }
+                    
+                    if(value < value2){
+                        sum -= value;
+                    }
+                    else{
+                        sum += value;
+                    }
+                   
+           }
+            
+            return sum;
+        }
+            
+
+    }
+

673db6a0530daa5c9e6d5a7ffde34f1580cec76b
Erik Chin <91162305+echin044@users.noreply.github.com>
2022-09-04 19:33:02 -0400

    Time: 0 ms (100.00%), Space: 40.6 MB (65.19%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int fib(int n) {
+        if(n == 0)
+            return 0;
+        
+        if(n == 1 | n == 2)
+            return 1;
+        
+        int a = 1;
+        int b = 1;
+        
+        //only uses 2 numbers at once
+        for(int i = 3; i<=n; i++){
+            int c = a;
+            a = b;
+            b = c + b;
+        }
+        
+        return b;
+    }
+}

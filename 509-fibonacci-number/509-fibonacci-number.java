c211c517355eb07e67e8a823dbc7d2b50d457476
Erik Chin <91162305+echin044@users.noreply.github.com>
2022-09-04 19:34:36 -0400

    Time: 0 ms (100.00%), Space: 39.3 MB (88.49%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,17 +1,12 @@
 class Solution {
-    
-    //topdown
+
     public int fib(int n) {
         if(n == 0)
             return 0;
         
-        if(n == 1 | n == 2)
-            return 1;
-        
         int a = 1;
         int b = 1;
         
-        //only uses 2 numbers at once
         for(int i = 3; i<=n; i++){
             int c = a;
             a = b;

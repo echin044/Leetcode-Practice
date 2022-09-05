8b8abe0a8e96e4e82726796024acd236ca7178d3
Erik Chin <91162305+echin044@users.noreply.github.com>
2022-09-05 19:23:43 -0400

    Time: 68 ms (5.12%), Space: 43.4 MB (6.06%) - LeetHub

---
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.java
@@ -0,0 +1,65 @@
+class Solution {
+    public boolean isRobotBounded(String instructions) {
+        
+        //direction based on X and Y axis
+        int leftDir = 0;
+        int topDir = 1;
+        
+        //position on X and Y axis
+        int positionX = 0;
+        int positionY = 0;
+        
+        for(int i = 0; i< 4; i++){
+        for(char c : instructions.toCharArray()){
+            
+            switch(c){
+                case 'G':
+                    positionX+= leftDir;
+                    positionY+= topDir;
+                    break;
+                case 'L':
+                    if(leftDir==0&topDir==1){
+                        leftDir = -1;
+                        topDir = 0;
+                    }
+                    else if(leftDir==1&topDir==0){
+                        leftDir = 0;
+                        topDir = 1;
+                    }
+                    else if(leftDir==0&topDir==-1){
+                        leftDir = 1;
+                        topDir = 0;
+                    }
+                    else if(leftDir==-1&topDir==0){
+                        leftDir = 0;
+                        topDir = -1;
+                    }
+                    break;
+                case 'R':
+                    if(leftDir==0&topDir==1){
+                        leftDir = 1;
+                        topDir = 0;
+                    }
+                    else if(leftDir==1&topDir==0){
+                        leftDir = 0;
+                        topDir = -1;
+                    }
+                    else if(leftDir==0&topDir==-1){
+                        leftDir = -1;
+                        topDir = 0;
+                    }
+                    else if(leftDir==-1&topDir==0){
+                        leftDir = 0;
+                        topDir = 1;
+                    }
+                    break;
+            }
+            System.out.println(leftDir + " " + topDir);
+        }
+            
+            
+        }
+        System.out.println(positionX + " " + positionY);
+    return positionX == 0 & positionY == 0;
+    }
+}

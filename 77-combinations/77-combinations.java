2f6a5dff5480b86005fc6208754927b2324f670b
Erik Chin <91162305+echin044@users.noreply.github.com>
2022-09-05 18:14:13 -0400

    Time: 19 ms (80.64%), Space: 54.8 MB (60.96%) - LeetHub

--- 77-combinations/77-combinations.java
+++ 77-combinations/77-combinations.java
@@ -1,16 +1,18 @@
 class Solution {
-     List<List<Integer>> ans = new ArrayList<>();
+     List<List<Integer>> result = new ArrayList<>();
     
     public List<List<Integer>> combine(int n, int k) {
         ArrayList<Integer> al = new ArrayList<Integer>();
         permute(n, 0,k,al);
-        return ans;
+        return result;
     }
     public void permute(int n, int index,int k,ArrayList<Integer> curr){
+        //when combination is at k numbers, add it to list of lists
         if(curr.size() == k){
-            ans.add(new ArrayList<>(curr));
+            result.add(new ArrayList<>(curr));
             return;
         }
+        
         for(int i = index ; i < n; i ++){
             curr.add(i+1);
             permute(n, i+1,k,curr);
